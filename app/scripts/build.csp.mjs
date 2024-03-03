0a7e4060cefaa3fdf08c81dbd7e0f6457f1fa7a9
David Dal Busco <david.dalbusco@outlook.com>
2024-03-03 11:27:31 +0100

    build: open csp image

--- app/scripts/build.csp.mjs
+++ app/scripts/build.csp.mjs
@@ -136,7 +136,7 @@ const updateCSP = (indexHtml) => {
         http-equiv="Content-Security-Policy"
         content="default-src 'none';
         connect-src 'self' https://ic0.app https://icp0.io https://icp-api.io;
-        img-src 'self' data:;
+        img-src 'self' data: https://*.blob.core.windows.net/;
         manifest-src 'self';
         script-src 'unsafe-eval' 'unsafe-inline' 'strict-dynamic' ${indexHashes.join(' ')};
         base-uri 'self';
